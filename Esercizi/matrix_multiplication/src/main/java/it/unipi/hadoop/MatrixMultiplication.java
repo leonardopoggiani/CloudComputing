f567f8d127040530f454cad794a95e808cf79db0
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2021-05-11 17:47:58 +0200

    matrix multiplication project

---
+++ Esercizi/matrix_multiplication/src/main/java/it/unipi/hadoop/MatrixMultiplication.java
@@ -0,0 +1,61 @@
+package it.unipi.hadoop;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.*;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+
+public class MatrixMultiplication
+{
+    public static class MatrixMultiplicationMapper extends Mapper<LongWritable, Text, Text, TimeSeriesData> {
+        public void map(final LongWritable key, final Text value, final Context context)
+                throws IOException, InterruptedException {
+        }
+    }
+}
+
+public static class MatrixMultiplicationReducer extends Reducer<Text, TimeSeriesData, Text, Text> {
+
+    public void reduce(final Text key, final Iterable<TimeSeriesData> values, final Context context)
+            throws IOException, InterruptedException {
+
+    }
+}
+
+    public static void main(final String[] args) throws Exception {
+        final Configuration conf = new Configuration();
+        final Job job = Job.getInstance(conf, "MatrixMultiplication");
+        job.setJarByClass(MatrixMultiplication.class);
+
+        // define mapper's output key-value
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(TimeSeriesData.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        job.setMapperClass(MovingAverageMapper.class);
+        job.setReducerClass(MovingAverageReducer.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
