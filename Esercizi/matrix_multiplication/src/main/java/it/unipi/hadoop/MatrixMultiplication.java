bacedcbd1d3e5d1e545518b782205034a684de97
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2021-05-14 00:40:57 +0200

    matrix_multiplication

--- Esercizi/matrix_multiplication/src/main/java/it/unipi/hadoop/MatrixMultiplication.java
+++ Esercizi/matrix_multiplication/src/main/java/it/unipi/hadoop/MatrixMultiplication.java
@@ -16,31 +16,76 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
 
 public class MatrixMultiplication
 {
     public static class MatrixMultiplicationMapper extends Mapper<LongWritable, Text, Text, Text> {
 
-        private final static Text mapperKey = new Text();
-        private final static Text reducerKey = new Text();
+        private final static Text outputKey = new Text();
+        private final static Text outputValue = new Text();
+
+        private final int columnsN = 100;
+        private final int rowsM = 100;
 
         public void map(final LongWritable key, final Text value, final Context context)
                 throws IOException, InterruptedException {
             String[] splittedRow = value.toString().split(",");
             // [0] -> nome matrice, [1] -> numero riga (i), [2] -> numero colonna (j), [3] -> valore
-            mapperKey.set(splittedRow[1] + "," + splittedRow[2]);
-            reducerKey.set(splittedRow[0] + ","  + splittedRow[3]);
 
-            context.write(mapperKey, reducerKey);
+            if (splittedRow[0].equals("M")) {
+                for(int k = 0; k < columnsN; k++) {
+                    outputKey.set(splittedRow[1] + "," + k);
+                    outputValue.set(splittedRow[0] + "," + splittedRow[2] + "," + splittedRow[3]);
+                    context.write(outputKey, outputValue);
+                }
+            } else {
+                for(int i = 0; i < rowsM; i++) {
+                    outputKey.set(i + "," + splittedRow[2]);
+                    outputValue.set(splittedRow[0] + "," + splittedRow[1] + "," + splittedRow[3]);
+                    context.write(outputKey, outputValue);
+                }
+            }
         }
     }
 
     public static class MatrixMultiplicationReducer extends Reducer<Text, Text, Text, Text> {
 
-        public void reduce(final Text key, final Text values, final Context context)
+        public void reduce(final Text key, final Iterable<Text> values, final Context context)
                 throws IOException, InterruptedException {
 
+            //  key -> (2,3) | (M, *, *) .. (N, *, *)
+
+            Map<Integer, Float> hashA = new HashMap<Integer, Float>();
+            Map<Integer, Float> hashB = new HashMap<Integer, Float>();
+
+            String[] arrayMat;
+            for(Text value : values) {
+                arrayMat = value.toString().split(",");
+                if(arrayMat[0].equals("M")){
+                    hashA.put(Integer.parseInt(arrayMat[1]),Float.parseFloat(arrayMat[2]));
+                } else {
+                    hashB.put(Integer.parseInt(arrayMat[1]),Float.parseFloat(arrayMat[2]));
+                }
+            }
+
+            float sum = 0;
+            float mij;
+            float njk;
+            for(int j = 0; j < 1000; j++) {
+                mij = hashA.containsKey(j) ? hashA.get(j) : 0;
+
+                njk  = hashB.containsKey(j) ? hashB.get(j) : 0;
+
+                sum += mij * njk;
+            }
+
+            Text result = new Text();
+            result.set(String.valueOf(sum));
+
+            if(sum != 0)
+                context.write(key, result);
         }
     }
 
@@ -62,8 +107,11 @@ public class MatrixMultiplication
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
 
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.out.println(args);
+
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path(args[2]));
+        FileOutputFormat.setOutputPath(job, new Path(args[3]));
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
