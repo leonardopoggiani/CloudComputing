2822c4b9937ae1479bea7f6998375b7fc2b2eaf4
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2021-05-14 12:24:25 +0200

    Matrice e tipo nullWritable

--- Esercizi/matrix_multiplication/src/main/java/it/unipi/hadoop/MatrixMultiplication.java
+++ Esercizi/matrix_multiplication/src/main/java/it/unipi/hadoop/MatrixMultiplication.java
@@ -1,14 +1,14 @@
 package it.unipi.hadoop;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.util.*;
+import java.util.Map;
+import java.util.HashMap;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -18,100 +18,102 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-
 public class MatrixMultiplication
 {
-    public static class MatrixMultiplicationMapper extends Mapper<LongWritable, Text, Text, Text> {
+    public static class MatrixMapper extends Mapper<LongWritable, Text, Text, Text>
+    {
+        private final Text outputKey   = new Text();
+        private final Text outputValue = new Text();
 
-        private final static Text outputKey = new Text();
-        private final static Text outputValue = new Text();
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
+        {
+            Configuration conf = context.getConfiguration();
+            final int NUM_COLS_N = Integer.parseInt(conf.get("num_cols_n"));
+            final int NUM_ROWS_M = Integer.parseInt(conf.get("num_rows_m"));
 
-        private final int columnsN = 100;
-        private final int rowsM = 100;
-
-        public void map(final LongWritable key, final Text value, final Context context)
-                throws IOException, InterruptedException {
-            String[] splittedRow = value.toString().split(",");
-            // [0] -> nome matrice, [1] -> numero riga (i), [2] -> numero colonna (j), [3] -> valore
-
-            if (splittedRow[0].equals("M")) {
-                for(int k = 0; k < columnsN; k++) {
-                    outputKey.set(splittedRow[1] + "," + k);
-                    outputValue.set(splittedRow[0] + "," + splittedRow[2] + "," + splittedRow[3]);
+            String line = value.toString();
+            // (M, i, j, Mij);
+            String[] indicesAndValue = line.split(",");
+            if (indicesAndValue[0].equals("M")) {
+                for (int k = 0; k < NUM_COLS_N; k++) {
+                    outputKey.set(indicesAndValue[1] + "," + k);
+                    outputValue.set("M" + "," + indicesAndValue[2] + "," + indicesAndValue[3]);
                     context.write(outputKey, outputValue);
                 }
             } else {
-                for(int i = 0; i < rowsM; i++) {
-                    outputKey.set(i + "," + splittedRow[2]);
-                    outputValue.set(splittedRow[0] + "," + splittedRow[1] + "," + splittedRow[3]);
+                // (N, j, k, Njk);
+                for (int i = 0; i < NUM_ROWS_M; i++) {
+                    outputKey.set(i + "," + indicesAndValue[2]);
+                    outputValue.set("N," + indicesAndValue[1] + "," + indicesAndValue[3]);
                     context.write(outputKey, outputValue);
                 }
             }
         }
     }
 
-    public static class MatrixMultiplicationReducer extends Reducer<Text, Text, Text, Text> {
-
-        public void reduce(final Text key, final Iterable<Text> values, final Context context)
-                throws IOException, InterruptedException {
-
-            //  key -> (2,3) | (M, *, *) .. (N, *, *)
-
+    public static class MatrixReducer extends Reducer<Text, Text, NullWritable, Text>
+    {
+        @Override
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException
+        {
+            // key=(i,k),
+            // Values = [(M/N,j,V/W),..]
             Map<Integer, Float> hashA = new HashMap<Integer, Float>();
             Map<Integer, Float> hashB = new HashMap<Integer, Float>();
-
-            String[] arrayMat;
-            for(Text value : values) {
-                arrayMat = value.toString().split(",");
-                if(arrayMat[0].equals("M")){
-                    hashA.put(Integer.parseInt(arrayMat[1]),Float.parseFloat(arrayMat[2]));
-                } else {
-                    hashB.put(Integer.parseInt(arrayMat[1]),Float.parseFloat(arrayMat[2]));
-                }
+            String[] value;
+            for (Text val : values) {
+                value = val.toString().split(",");
+                if (value[0].equals("M"))
+                    hashA.put(Integer.parseInt(value[1]), Float.parseFloat(value[2]));
+                else
+                    hashB.put(Integer.parseInt(value[1]), Float.parseFloat(value[2]));
             }
-
-            float sum = 0;
-            float mij;
-            float njk;
-            for(int j = 0; j < 1000; j++) {
-                mij = hashA.containsKey(j) ? hashA.get(j) : 0;
-
-                njk  = hashB.containsKey(j) ? hashB.get(j) : 0;
-
-                sum += mij * njk;
+            int NUM_COLS_M = Integer.parseInt(context.getConfiguration().get("num_cols_m"));
+            float result = 0.0f;
+            float m_ij;
+            float n_jk;
+            for (int j = 0; j < NUM_COLS_M; j++) {
+                m_ij = hashA.containsKey(j) ? hashA.get(j) : 0.0f;
+                n_jk = hashB.containsKey(j) ? hashB.get(j) : 0.0f;
+                result += m_ij * n_jk;
             }
-
-            Text result = new Text();
-            result.set(String.valueOf(sum));
-
-            if(sum != 0)
-                context.write(key, result);
+            if (result != 0.0f)
+                context.write(null, new Text(key.toString() + "," + Float.toString(result)));
         }
     }
 
-    public static void main(final String[] args) throws Exception {
-        final Configuration conf = new Configuration();
-        final Job job = Job.getInstance(conf, "MatrixMultiplication");
-        job.setJarByClass(MatrixMultiplication.class);
+    public static void main(String[] args) throws Exception
+    {
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 3) {
+            System.err.println("Usage: MatrixMultiplication <input> <output>");
+            System.exit(1);
+        }
+        System.out.println("args[0]: <input>="  + otherArgs[1]);
+        System.out.println("args[1]: <output>=" + otherArgs[2]);
 
-        // define mapper's output key-value
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(Text.class);
+        Job job = Job.getInstance(conf, "MatrixMultiplication");
+        job.getConfiguration().set("num_rows_m", "100");
+        job.getConfiguration().set("num_cols_m", "1000");
+        job.getConfiguration().set("num_rows_n", "1000");
+        job.getConfiguration().set("num_cols_n", "100");
+
+        job.setJarByClass(MatrixMultiplication.class);
+        job.setMapperClass(MatrixMapper.class);
+        job.setReducerClass(MatrixReducer.class);
+
+        job.setNumReduceTasks(3);
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
 
-        job.setMapperClass(MatrixMultiplicationMapper.class);
-        job.setReducerClass(MatrixMultiplicationReducer.class);
+        FileInputFormat.addInputPath(job, new Path(otherArgs[1]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
 
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
-
-        System.out.println(args);
-
-        FileInputFormat.addInputPath(job, new Path(args[1]));
-        FileInputFormat.addInputPath(job, new Path(args[2]));
-        FileOutputFormat.setOutputPath(job, new Path(args[3]));
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
