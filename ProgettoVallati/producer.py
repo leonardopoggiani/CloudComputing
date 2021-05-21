05cf799d2ae587c00c06613950955e982ed3e420
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2021-05-21 22:06:33 +0200

    aggiunto file producer e rinominata cartella

---
+++ ProgettoVallati/producer.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataframe = pd.read_csv("C:\\Users\Leonardo Poggiani\\Documents\\GitHub\\covid19-opendata-vaccini\\dati\\vaccini-summary-latest.csv")
+
+plt.plot(dataframe['area'], dataframe['dosi_somministrate'], 'r--')
+plt.plot(dataframe['area'], dataframe['dosi_consegnate'])
+plt.xticks(rotation=35)
+plt.grid()
+
+plt.show()
+
+dataframe['differenza_consegnate_somministrate'] = dataframe['dosi_consegnate'] - dataframe['dosi_somministrate']
+print(dataframe[['area', 'dosi_somministrate', 'differenza_consegnate_somministrate']])
+
+plt.plot(dataframe['area'], dataframe['differenza_consegnate_somministrate'])
+plt.xticks(rotation=35)
+plt.grid()
+
+plt.show()
+
+    dataframe = pd.read_csv("C:\\Users\Leonardo Poggiani\\Documents\\GitHub\\covid19-opendata-vaccini\\dati\\somministrazioni-vaccini-latest.csv")
