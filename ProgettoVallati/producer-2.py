a91f5959256ae2016cf9b44f146709dd6d5be3d6
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2021-05-30 17:58:29 +0200

    producer 2 added

--- ProgettoVallati/producer-2.py
+++ ProgettoVallati/producer-2.py
@@ -4,6 +4,7 @@ import json
 import pandas as pd
 
 if __name__ == "__main__":
+<<<<<<< Updated upstream
     '''
     auth = v3.Password(auth_url='http://172.16.50.247:5000/v3',
                        username='admin',
@@ -16,11 +17,16 @@ if __name__ == "__main__":
     '''
 
     #os.system("sh admin-openrc.sh")
+=======
+
+    os.system("sh admin-openrc.sh")
+>>>>>>> Stashed changes
     os.system("openstack token issue > output_token.txt")
     os.system("cat output_token.txt | grep \"gAAA[^[:space:]]*\" -o > token.txt")
 
     with open("token.txt") as token_file:
         token = token_file.readlines()
+<<<<<<< Updated upstream
         str_token = token.pop().replace('\n', '')
         headers = {'content-type': 'application/json', 'X-AUTH-TOKEN': str_token}
     while True:
@@ -100,3 +106,33 @@ if __name__ == "__main__":
 
     #Ottenere la lista delle policy
     #curl - H "X-AUTH-TOKEN:"http: // 252.3.243.35: 8041 / v1 / archive_policy
+=======
+
+        str_token = token.pop().replace('\n', '')
+
+        url = "http://252.3.243.35:8041/v1/metric/67d112e8-4d3c-4ae6-8577-0175ac48da1d/measures"
+
+        headers = {'content-type': 'application/json', 'X-AUTH-TOKEN': str_token}
+
+    df = pd.read_csv("data/csv/temperature_2017.csv")
+
+    rng = pd.date_range('20210501 00:00', '20210701 23:59', freq='5S')
+    index = 0
+
+    for line in df.itertuples():
+        timestamp, value = [], []
+        value.append(float(line[2]))
+        timestamp.append(str(rng.to_series()[index]))
+        index = index + 1
+        measures = [{"timestamp": t, "value": v} for t, v in zip(timestamp, value)]
+        r = requests.post(url, data=json.dumps(measures), headers=headers)
+        # if str(r.status_code) == "202":
+            # print("inserted measure")
+        # else:
+            # print(str(r.status_code))
+        if index == len(rng):
+             break
+
+# ID METRIC 67d112e8-4d3c-4ae6-8577-0175ac48da1d
+
+>>>>>>> Stashed changes
--- ProgettoVallati/producer.py
+++ ProgettoVallati/producer.py
@@ -18,28 +18,32 @@ if __name__ == "__main__":
                        project_name='admin',
                        user_domain_id='default',
                        project_domain_id='default')
+
     sess = session.Session(auth=auth)
     token = sess.get_token()
 
     url = "http://252.3.243.35:8041/v1/metric/"+sys.argv[2]+"/measures"
-    headers = {'content-type': 'application/json', 'X-AUTH-TOKEN': sys.argv[1]}
+    headers = {'content-type': 'application/json', 'X-AUTH-TOKEN': token}
 
-    with open(sys.argv[3], "r") as lines:
-        rng = pd.date_range('20210301 00:00', '20210610 23:59', freq='1S')
-        index = 0
-        for line in lines:
-            timestamp, value = [], []
-            value.append(float(line.split(" ")[2]))
-            timestamp.append(str(rng.to_series()[index]))
-            index = index+1
-            measures = [{"timestamp": t, "value": v} for t, v in zip(timestamp, value)]
-            r = requests.post(url, data=json.dumps(measures), headers=headers)
-            if str(r.status_code) == "202":
-                print("inserted measure")
-            else:
-                print(str(r.status_code))
-            if index == len(rng):
-                break
-            print(index)
+    df = pd.read_csv("data/csv/temperature_2017.csv")
+
+    rng = pd.date_range('20210301 00:00', '20210610 23:59', freq='1S')
+    index = 0
+
+    for line in df.itertuples():
+        timestamp, value = [], []
+        value.append(float(line[2]))
+        timestamp.append(str(rng.to_series()[index]))
+        index = index + 1
+        measures = [{"timestamp": t, "value": v} for t, v in zip(timestamp, value)]
+        print(measures)
+        r = requests.post(url, data=json.dumps(measures), headers=headers)
+        if str(r.status_code) == "202":
+            print("inserted measure")
+        else:
+            print(str(r.status_code))
+        if index == len(rng):
+            break
+        print(index)
 
 #ID METRIC 67d112e8-4d3c-4ae6-8577-0175ac48da1d
