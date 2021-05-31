cb1da5abe5d8bbd9ca97dc27f77c907c91026269
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2021-05-31 09:01:55 +0200

    excuting admin-openrc.sh directly from producer

--- ProgettoVallati/producer-2.py
+++ ProgettoVallati/producer-2.py
@@ -24,7 +24,18 @@ if __name__ == "__main__":
                "5) Insert measures in a metric\n" + \
                "6) Exit\n "
 
-    os.system("export OS_PROJECT_ID=9009edd7c5104c628d8ed9b16bf5ec31")  # autentica attraverso lo script prodotto da openstack
+    # eseguo i comandi di admin-openrc.sh
+    os.environ["OS_PROJECT_ID"] = "9009edd7c5104c628d8ed9b16bf5ec31"
+    os.environ["OS_AUTH_URL"] = "http://252.3.243.14:5000/v3"
+    os.environ["OS_PROJECT_NAME"] = "admin"
+    os.environ["OS_USER_DOMAIN_NAME"] = "admin_domain"
+    os.environ["OS_PROJECT_DOMAIN_ID"] = "5227438031ac4bf2a8f1c3fcba908e94"
+    os.environ["OS_USERNAME"] = "admin"
+    os.environ["OS_PASSWORD"] = "openstack"
+    os.environ["OS_REGION_NAME"] = "RegionOne"
+    os.environ["OS_INTERFACE"] = "public"
+    os.environ["OS_IDENTITY_API_VERSION"] = "3"
+
     os.system("openstack token issue > output_token.txt")  # salva il token di accesso in file di testo
     os.system(
         "cat output_token.txt | grep \"gAAA[^[:space:]]*\" -o > token.txt")  # recupera solamente il token e lo salva su un altro file
